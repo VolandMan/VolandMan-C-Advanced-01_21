c71fbaf0f40bbb7a5f8e6497cfb82868737ab4d1
Voland <42006964+VolandMan@users.noreply.github.com>
2021-04-18 14:55:52 +0300

    Add files via upload

---
+++ Home_3/Delegate1/Delegate1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Delegate1
+{
+    class Program
+    {
+        int a, b, c;
+        public delegate double Mydelegate (int a, int b, int c); 
+
+        public static double Average (int a, int b, int c)
+        {
+            return (a + b + c) / 3;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите аргумент 1 =");
+
+            int a = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите аргумент 2 =");
+
+            int b = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Введите аргумент 3 =");
+
+            int c = Int32.Parse(Console.ReadLine());
+
+
+
+            Mydelegate mydelegate1 = new Mydelegate(Average);
+            Console.WriteLine($"Average {a},{b},{c}  = "+ mydelegate1.Invoke(a, b, c));
+
+            Console.WriteLine(new string('-', 10));
+
+
+            Mydelegate mydelegate2 = delegate (int a, int b, int c) {return (a + b + c) / 3; };
+            double average = mydelegate2.Invoke(a, b, c);
+            Console.WriteLine($"Anonymous method Average = {a},{b},{c} = " + average);
+           
+            
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Home_3/Delegate2/Delegate2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Delegate2
+{
+    class Program
+    {
+        public delegate int MyDelegat(int num1, int num2);
+        static void Main()
+        {
+           
+            Console.WriteLine("Первое число = ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Второе число = ");
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Выбор операции  +,-,*,/");
+            string a = Console.ReadLine();
+
+            switch (a)
+            {
+                case "+":
+                    MyDelegat myDelegat = (num1, num2) => { return num1 + num2; };
+                    int sum = myDelegat(x, y);
+                    Console.WriteLine("Сумма чисел:{0}", sum);
+                    break;
+                case "-":
+                    MyDelegat myDelegat1 = (num1, num2) => { return num1 - num2; };
+                    int sub = myDelegat1(x, y);
+                    Console.WriteLine("Результат вычитания:{0}", sub);
+                    break;
+                case "*":
+                    MyDelegat myDelegat2 = (num1, num2) => { return num1 * num2; };
+                    int mul = myDelegat2(x, y);
+                    Console.WriteLine("езультат умнодения:{0}", mul);
+                    break;
+                case "/":
+                    MyDelegat myDelegat3 = (num1, num2) => { return num1 / num2; };
+                    int div = myDelegat3(x, y);
+                    Console.WriteLine("Результат деления:{0}", div);
+                    break;
+                default:
+                    Console.WriteLine("Выберите из предложанного");
+                    break;
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
