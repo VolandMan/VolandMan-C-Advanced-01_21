b17fb9bec7c8e7a80711866f3cca8ab9b15dbb80
Voland <42006964+VolandMan@users.noreply.github.com>
2021-05-04 13:29:15 +0300

    Add files via upload

---
+++ Home_4/Calculator_dynamic/Calculator/Calculator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculator
+{
+    class Calculator <T1, T2> 
+    {
+        
+        public static double Add(T1 t,  T2 t2)
+            {
+                var tValue = t as dynamic; 
+                var tValue2 = t as dynamic;
+           
+                return (double)tValue + tValue2;
+            }
+
+        public static double Subtraction (T1 t, T2 t2)
+        {
+            var tValue = t as dynamic;
+            var tValue2 = t as dynamic;
+
+            return (double)tValue - tValue2;
+        }
+
+        public static double Multiplication(T1 t, T2 t2)
+        {
+            var tValue = t as dynamic;
+            var tValue2 = t as dynamic;
+
+            return (double)tValue * tValue2;
+        }
+
+
+    }
+}
---
+++ Home_4/Calculator_dynamic/Calculator/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b;
+            char z;
+            Console.WriteLine("Введите a ");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите действие (+, - , *,) ");
+            z = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine("Введите b ");
+            b = Convert.ToDouble(Console.ReadLine());
+            switch (z)
+            {
+                case '+':
+                    Console.WriteLine("{0}+{1}={2}", a, b, Calculator<double, double>.Add(a, b));
+                    break;
+                case '-':
+                    Console.WriteLine("{0}-{1}={2}", a, b, Calculator<double, double>.Subtraction(a, b));
+                    break;
+                case '*':
+                    Console.WriteLine("{0}*{1}={2}", a, b, Calculator<double, double>.Multiplication(a, b));
+                    break;
+    
+                default:
+                    Console.WriteLine("Ошибка");
+                    break;
+            }
+            Console.ReadLine();
+                       
+     
+        }
+    }
+}
