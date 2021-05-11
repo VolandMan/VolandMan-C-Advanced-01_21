0265b37072637138d0306f63213c9852b3677034
Voland <42006964+VolandMan@users.noreply.github.com>
2021-05-11 14:32:46 +0300

    Add files via upload

---
+++ Home_5/CodeFirst_Many-Many/ManyToManyEntity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace CodeFirst_M_M
+{
+    public class ManyToManyEntity : DbContext
+    {
+   
+        public ManyToManyEntity()
+            : base("name=ManyToManyEntity")
+        {
+        }
+
+
+         public virtual DbSet<Person> PersonEntity { get; set; }
+         public virtual DbSet<Projects> ProjectEntity { get; set; }
+         public virtual DbSet<Phone> Phone { get; set; }
+
+    }
+}
