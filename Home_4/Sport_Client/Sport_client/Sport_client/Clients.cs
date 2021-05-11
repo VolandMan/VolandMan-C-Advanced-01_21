fa811a03308e5fff7f6c65caf2246cdc051c7fe1
Voland <42006964+VolandMan@users.noreply.github.com>
2021-05-11 13:34:04 +0300

    Add files via upload

---
+++ Home_4/Sport_Client/Sport_client/Sport_client/Clients.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sport_client
+{
+    class Clients
+    {
+        public int Id { get; set; }
+
+        public int Year { get; set; }
+
+        public int Month { get; set; }
+
+        public int Time { get; set; }
+    }
+}
---
+++ Home_4/Sport_Client/Sport_client/Sport_client/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace Sport_client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Clients> clients = new List<Clients>()
+            {
+                new Clients
+                {
+                    Id = 1,
+                    Year = 2000,
+                    Month = 11,
+                    Time = 19
+                },
+                new Clients()
+                {
+                    Id = 2,
+                    Year = 1999,
+                    Month = 10,
+                    Time = 21
+                },
+                new Clients()
+                {
+                    Id = 3,
+                    Year = 2001,
+                    Month = 8,
+                    Time = 26
+                },
+                new Clients()
+                {
+                    Id = 4,
+                    Year = 1998,
+                    Month = 7,
+                    Time = 28
+                }
+            };
+
+
+
+            
+            var q = clients.GroupBy(x => x.Time).OrderBy(x => x.Key).First().Select(x => new { x.Time, x.Year, x.Month }).Last();
+            Console.WriteLine("{0}, {1}, {2}", q.Time, q.Year, q.Month);
+
+
+        }
+
+    }
+}
+    
